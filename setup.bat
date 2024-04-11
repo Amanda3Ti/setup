86f49451d1d3eb9af23ba521e32adc3550d2b442
Amanda3Ti <00001065225350SP@aluno.educacao.sp.gov.br>
2024-04-11 11:52:28 -0300

    setup feito

---
+++ setup.bat
@@ -0,0 +1,9 @@
+git config --global user.name "Amanda3Ti"
+git config --global user.email "00001065225350SP@aluno.educacao.sp.gov.br"
+
+cd C:\Users\AMANDAPEREIRADASILVA\
+
+git clone https://github.com/Amanda3Ti/Caeser-Cipher.git
+cd Caeser-Cipher
+
+code .
